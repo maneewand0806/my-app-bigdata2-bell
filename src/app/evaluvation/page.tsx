2dd8cc8f638a52a535cc0dacd56db93c324bcc8a
PIYAPHON2 <piyaphon.ja@ku.th>
2024-08-24 17:32:35 +0700

    first commit

---
+++ src/app/evaluvation/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Evaluvation from '@/components/Evaluvation';
+export default function page() {
+  return (
+    <div>
+      <Evaluvation />
+    </div>
+  );
+}
---
+++ src/app/produclist/page.js
@@ -0,0 +1,10 @@
+import Produclist from '@/components/Produclist'
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>
+        <Produclist/>
+    </div>
+  )
+}
